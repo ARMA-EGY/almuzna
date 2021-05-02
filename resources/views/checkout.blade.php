586bd0d8fae84623bc4263a210568107feede7a4
youssef97hesham <48891247+youssef97hesham@users.noreply.github.com>
2021-05-02 04:12:32 +0200

    mm

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -826,7 +826,6 @@
 
 
 
-
         <!-- Location Modal -->
         <div class="modal fade" id="LocationModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
